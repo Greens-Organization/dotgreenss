02457721bab602afe7bafe88777d734005020600
Matheus P Agostinho <contact@matheuspa.com>
2023-08-24 18:49:04 -0300

    alterado formato do arquivo startup.sh de CR para CRLF

--- games/minecraft/start-server-automatic/startup.sh
+++ games/minecraft/start-server-automatic/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Inicia o TMUX e cria uma sessão chamada "Minecraft"
-tmux new-session -d -s Minecraft
+tmux new-session -d -s minecraft
 
 # Executa um comando na sessão "Minecraft" (substitua o comando pelo que você deseja executar)
-tmux send-keys -t Minecraft 'cd ServerGreensCreate/' C-m
+tmux send-keys -t minecraft 'cd minecraft/' C-m
 
 # Executa o comando para iniciar o servidor do game
-tmux send-keys -t Minecraft './start-reboot.sh' C-m
+tmux send-keys -t minecraft 'bash reboot' C-m
 
 # Anexa à sessão "Minecraft" para visualizá-la
 tmux attach-session -t Minecraft
